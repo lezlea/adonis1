1e74463bcd166ff42b34a4ab4a63442bc8bba17c
Lea <hello@lezlea.co.uk>
2024-01-30 17:09:15 +0800

    up to 24 layouts

--- app/controllers/posts_controller.ts
+++ app/controllers/posts_controller.ts
@@ -2,7 +2,7 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class PostsController {
     public index = async ({response,view}:HttpContext) => {
-       const html = await view.render('posts/index')
+       const html = await view.render('posts/index',{title: 'Post Index Page'})
         return html;
         //return response.status(200).json('Okay it is working')
     }
